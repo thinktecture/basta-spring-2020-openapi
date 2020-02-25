99ae5796bcdb89ad943798e388c2862601b3cdff
Unknown <sebastian.gingter@thinktecture.com>
2020-02-25 09:36:33 +0100

    Prepared Models

---
+++ src/OpenApi/OpenApi/Models/Article.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OpenApi.Models
+{
+	/// <summary>
+	/// Represents an article.
+	/// </summary>
+	public class Article
+	{
+		/// <summary>
+		/// The unique number of the article.
+		/// </summary>
+		/// <example>5</example>
+		[Required]
+		public int Number { get; set; }
+
+		/// <summary>
+		/// The name of the article.
+		/// </summary>
+		/// <example>Water</example>
+		[Required]
+		public string Name { get; set; }
+
+		/// <summary>
+		/// An optional description of the article.
+		/// </summary>
+		/// <example>A non-alcoholic beverage</example>
+		public string Description { get; set; }
+
+		/// <summary>
+		/// The price of the article.
+		/// </summary>
+		/// <example>1</example>
+		[Required]
+		public float Price { get; set; }
+	}
+}
--- src/OpenApi/OpenApi/Models/WeatherForecast.cs
+++ src/OpenApi/OpenApi/Models/WeatherForecast.cs
@@ -2,14 +2,33 @@ using System;
 
 namespace OpenApi.Models
 {
+	/// <summary>
+	/// Represents a weather forecast.
+	/// </summary>
 	public class WeatherForecast
 	{
+		/// <summary>
+		/// The date this forecast applies to.
+		/// </summary>
+		/// <example>2019-09-30T14:03:04.8060171+02:00</example>
 		public DateTime Date { get; set; }
 
+		/// <summary>
+		/// The forecast temperature in degrees Celsius.
+		/// </summary>
+		/// <example>25</example>
 		public int TemperatureC { get; set; }
 
+		/// <summary>
+		/// The forecast temperature in degrees Fahrenheit.
+		/// </summary>
+		/// <example>77</example>
 		public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
+		/// <summary>
+		/// A brief textual description of the forecast.
+		/// </summary>
+		/// <example>Mild</example>
 		public string Summary { get; set; }
 	}
 }
